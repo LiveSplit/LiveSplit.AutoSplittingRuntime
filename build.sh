38a76358cfa010926d46a72bece591b14ead5185
wooferzfg <spapushin@gmail.com>
2023-06-17 15:30:34 -0400

    Release v0.0.4

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd asr-capi
-cargo +stable-x86_64-pc-windows-gnu build --release
+cargo +stable-x86_64-pc-windows-msvc build --release
 cp target/release/asr_capi.dll ../x64/.
-cargo +stable-i686-pc-windows-gnu build --release
+cargo +stable-i686-pc-windows-msvc build --release
 cp target/release/asr_capi.dll ../x86/.
