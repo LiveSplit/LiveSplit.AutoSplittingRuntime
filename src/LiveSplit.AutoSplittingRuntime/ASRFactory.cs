0256baf2edc1d12b087b1c91ee2aff1ddaab8e76
wooferzfg <spapushin@gmail.com>
2025-09-29 10:51:57 -0400

    Update version to 0.0.11

--- src/LiveSplit.AutoSplittingRuntime/ASRFactory.cs
+++ src/LiveSplit.AutoSplittingRuntime/ASRFactory.cs
@@ -13,7 +13,7 @@ public class ASRFactory : IComponentFactory
     public string ComponentName => "Auto Splitting Runtime";
     public string Description => "Allows auto splitters provided as WebAssembly modules to define the splitting behaviour.";
     public ComponentCategory Category => ComponentCategory.Control;
-    public Version Version => Version.Parse("0.0.10");
+    public Version Version => Version.Parse("0.0.11");
 
     public string UpdateName => ComponentName;
     public string UpdateURL => "http://livesplit.org/update/";
